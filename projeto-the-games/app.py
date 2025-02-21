ef32a11178a2123c6370f11e2ad84035580acc10
anardes <anardesmotta@gmail.com.br>
2025-02-21 11:46:40 -0300

    @anardes

--- projeto-the-games/app.py
+++ projeto-the-games/app.py
@@ -1,15 +1,27 @@
-from flask import Flask #importando pacote do flask
+from flask import Flask, render_template#importando pacote do flask
 
-app = Flask (__name__) #carregando flask na variável app
+app = Flask (__name__, template_folder='views') #carregando flask na variável app
 
 #criando a rota principal do site
 @app.route('/')
 #criando função no python
 def home():
-    return '<h1>Meu primeiro site em Flask. Seja bem-vindo!</h1>'
+    return render_template('index.html')
 
 @app.route('/games')
+#criando função no python
 def games():
-    return '<h1>Seja bem-vindo à página de games</h1>'
+    titulo='CS-GO'
+    ano=2012
+    categoria='FPS Online'
+    jogadores=['iruah', 'davi_lambari', 'edsongf1', 'kioto', 'jujudopix']
+    jogos=['subway surfers', 'free fire', 'fortnite', 'cyberpunk', 'pou', 'my talking tom', 'plant vs zombie']
+    return render_template('games.html',
+                           titulo=titulo,
+                           ano=ano,
+                           categoria=categoria,
+                           jogadores=jogadores,
+                           jogos=jogos)
+
 if __name__ == '__main__': #rodando o servidor no localhost, porta 5000
     app.run(host = 'localhost', port=5000, debug = True)
